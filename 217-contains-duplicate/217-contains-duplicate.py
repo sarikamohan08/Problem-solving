74ee43edb550f87c2c91f5d2fae6eff91bbccebd
Sarika Mohanraj <66568393+sarikamohan08@users.noreply.github.com>
2022-02-28 22:22:26 +0530

    Time: 519 ms (53.55%), Space: 24 MB (44.51%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        """
+        n=len(nums)
+        arr=nums
+        freq=[0]*100000000
+        for i in range(n):
+            freq[arr[i]]+=1
+
+        for i in range(100000000):
+            if(freq[i]>0):
+                print(i,freq[i])
+
+        flag=0
+        for i in range(100000000):
+            if(freq[i]>1):
+                flag=1
+                break
+        if(flag==1):
+            return True
+        else:
+            return False
+        """
+        return True if len(set(nums)) < len(nums) else False
