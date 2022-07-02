92ae4b7a61aebd44a756d32cb834f27e81448946
Sarika Mohanraj <66568393+sarikamohan08@users.noreply.github.com>
2022-07-02 21:39:28 +0530

    Time: 59 ms (73.94%), Space: 17.4 MB (12.20%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,7 +1,7 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         import re
-        #s1=re.sub(r'[^\w\s]', '', s)
+        s1=re.sub(r'[^\w\s]', '', s)
         s1 = s.replace(' ', '')
         s1 = s1.lower()
         s1 = [x for x in s1 if x.isalnum()]
