35c789830085bdb6e1a0aa0be5f2ea6fc24731ae
Sarika Mohanraj <66568393+sarikamohan08@users.noreply.github.com>
2022-07-05 19:55:18 +0530

    Time: 291 ms (27.18%), Space: 14.7 MB (59.74%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -4,8 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        zero = 0
+        zero_place = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[i], nums[zero] = nums[zero], nums[i]
-                zero += 1
+                nums[i], nums[zero_place] = nums[zero_place], nums[i]
+                zero_place += 1
