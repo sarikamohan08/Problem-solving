822673af1f6e4aa433fd7a7716af88e9ea8e0266
Sarika Mohanraj <66568393+sarikamohan08@users.noreply.github.com>
2023-03-03 22:07:52 +0530

    Time: 26 ms (93.35%), Space: 13.9 MB (11.03%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -5,6 +5,7 @@ class Solution:
                     return 0
         
         if needle in haystack:
+            print(haystack.index(needle))
             return haystack.index(needle)
         else:
             return -1
